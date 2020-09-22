ec228019faf72c4a7255c8be4c790c1a409cd557
Luis Torres <luistjr@gmail.com>
2020-09-22 21:15:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -15,8 +15,8 @@ def state_log(star_date)
 end
 
 def engage
-  puts state_log(star_date)
-  date = generate_star_date
+  puts state_log(date)
+  generate_star_date = date
   binding.pry
   puts 'hello'
 end
