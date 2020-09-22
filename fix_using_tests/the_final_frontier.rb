f66526bfaca0fec82e2e1513f6f5d92520538391
Luis Torres <luistjr@gmail.com>
2020-09-22 21:11:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -10,10 +10,12 @@ end
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
   binding.pry
+  puts 'hello'
 end
 
 def engage
   puts state_log(date)
   date = generate_star_date
   binding.pry
+  puts 'hello'
 end
